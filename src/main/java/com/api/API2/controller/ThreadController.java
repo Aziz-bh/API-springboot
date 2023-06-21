a86192bb564c834506f7f853a0ecbbcdf4734707
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-06-21 01:08:24 +0100

    Major fixes for the thread controller

--- src/main/java/com/api/API2/controller/ThreadController.java
+++ src/main/java/com/api/API2/controller/ThreadController.java
@@ -6,6 +6,7 @@ import com.api.API2.entity.User;
 import com.api.API2.service.ThreadService;
 import com.api.API2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +27,10 @@ public class ThreadController {
     }
     @GetMapping("/threads")
     public ResponseEntity<Object> getAllThreads(HttpServletRequest request){
-
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || authorizationHeader.isBlank()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         User u=userService.getUserById(request.getHeader("Authorization").substring(7));
         if(u!=null){
             List<Thread> threads = threadService.getAllThreads();
@@ -66,6 +70,10 @@ public class ThreadController {
 
     @PostMapping("/thread")
     public ResponseEntity<Object> saveThread(@Valid @RequestBody Thread t,HttpServletRequest request){
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || authorizationHeader.isBlank()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         User u=userService.getUserById(request.getHeader("Authorization").substring(7));
         if(u!=null){
             t.setUser(u);
@@ -74,6 +82,11 @@ public class ThreadController {
     }
     @PutMapping("/thread/{id}")
     public ResponseEntity<Object> updateThread(@PathVariable String id, @Valid @RequestBody Thread t, HttpServletRequest request) {
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || authorizationHeader.isBlank()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         User user = userService.getUserById(request.getHeader("Authorization").substring(7));
 
         if (user != null) {
@@ -94,6 +107,11 @@ public class ThreadController {
 
     @DeleteMapping("/thread/{id}")
     public ResponseEntity<Object> deleteThread(@PathVariable String id,HttpServletRequest request){
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || authorizationHeader.isBlank()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
         User u=userService.getUserById(request.getHeader("Authorization").substring(7));
         if(u!=null) {
           if(  threadService.getThreadById(id).get().getUser().getId().equals(u.getId()))
